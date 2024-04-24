1b959e9479cc8f8fd36bf639c11a1f74e6af9ec0
duroshegc <130710270+duroshegc@users.noreply.github.com>
2024-04-24 18:33:22 -0500

    Initial heap

--- Assign9/heap.h
+++ Assign9/heap.h
@@ -3,8 +3,9 @@
 
 #include <algorithm>
 #include <stdexcept>
+#include <utility>
 
-using namspace std;
+using namespace std;
 
 inline constexpr size_t heap_left(size_t node) { 
   size_t left = 2 * node + 1;
@@ -12,6 +13,7 @@ inline constexpr size_t heap_left(size_t node) {
 }
 inline constexpr size_t heap_right(size_t node) { 
   size_t right = 2 * node + 2;
+  return right;
 }
 inline constexpr size_t heap_parent(size_t node) {
   if(node <= 0)
@@ -21,6 +23,7 @@ inline constexpr size_t heap_parent(size_t node) {
   else
   {
     size_t parent = (node - 1)/ 2;
+    return parent;
   }
 
 
@@ -28,46 +31,173 @@ inline constexpr size_t heap_parent(size_t node) {
 
 template <typename ArrayLike, typename FN>
 void heap_preorder(ArrayLike heapdata, size_t heapnodes, size_t node, FN fn) {
-  
+  if(node < heapnodes)
+  {
+    fn(node);
+    heap_preorder(heapdata, heapnodes, (2 * node) + 1, fn);
+    heap_preorder(heapdata, heapnodes, (2 * node) + 2, fn);
+  }
 }
 
 template <typename ArrayLike, typename FN>
 void heap_inorder(ArrayLike heapdata, size_t heapnodes, size_t node, FN fn) {
-  // XXX You must implement this.
+  if(node < heapnodes)
+  {
+    heap_inorder(heapdata, heapnodes, (2 * node) + 1, fn);
+    fn(node);
+    heap_inorder(heapdata, heapnodes, (2 * node) + 2, fn);
+  }
 }
 
 template <typename ArrayLike, typename FN>
 void heap_postorder(ArrayLike heapdata, size_t heapnodes, size_t node, FN fn) {
-  // XXX You must implement this.
+  if(node < heapnodes)
+  {
+    heap_postorder(heapdata, heapnodes, (2 * node) + 1, fn);
+    heap_postorder(heapdata, heapnodes, (2 * node) + 2, fn);
+    fn(node);
+  }
 }
 
 template <typename ArrayLike, typename FN>
 void heap_levelorder(ArrayLike & heapdata, size_t heapnodes, FN fn) {
-  // XXX You must implement this.
+  for(size_t i = 0; i < heapnodes; i++)
+  {
+    fn(heapdata[i]);
+  }
 }
 
 // min tree uses less than, so compare(parent, child) should be true
 template <typename ArrayLike, typename COMPARISON>
 bool is_heap(const ArrayLike & heapdata, size_t nodes, COMPARISON compare) { 
-  // XXX You must implement this properly.
-  return false;
+    for (size_t i = 0; i <= (nodes / 2) - 1; i++) {
+        size_t left_child_index = 2 * i + 1;
+        size_t right_child_index = 2 * i + 2;
+
+        // Check if left child exists and if it violates the heap property
+        if (left_child_index < nodes && !compare(heapdata[i], heapdata[left_child_index])) {
+            return false;
+        }
+
+        // Check if right child exists and if it violates the heap property
+        if (right_child_index < nodes && !compare(heapdata[i], heapdata[right_child_index])) {
+            return false;
+        }
+    }
+    return true;
 }
 
 template <typename RAIterator, typename COMPARE>
 size_t heap_bubble_up(RAIterator begin, size_t nodes, size_t start, COMPARE compare) {
-  // XXX You must implement this properly.
-  return 9999999;
+  auto child_index = start;
+  auto child = begin + child_index;
+  size_t parent_index = (start - 1)/ 2;
+  auto parent = begin + parent_index;
+  size_t swaps = 0;
+  
+  while(child_index > 0 && compare(*child, *parent))
+  {
+      swap(*child, *parent);
+      child_index = parent_index;
+      child = parent;
+      parent_index = (parent_index - 1)/ 2;
+      parent = begin + parent_index;
+      swaps++; 
+  }
+
+  return swaps;
+
 }
 
 template <typename RAIterator, typename COMPARE>
 size_t heap_bubble_down(RAIterator begin, size_t nodes, size_t start, COMPARE compare) {
-  // XXX You must implement this properly.
-  return 9999999;
+  auto parent_index = start;
+  auto parent = begin + parent_index;
+  auto left_child_index = 2 * parent_index + 1;
+  auto left_child = begin + left_child_index;
+  auto right_child_index = 2 * parent_index + 2;
+  auto right_child = begin + right_child_index;
+  size_t swaps = 0;
+  RAIterator ans;
+  size_t ans_index;
+
+  if(right_child_index >= nodes)
+  {
+    ans = left_child;
+    ans_index = left_child_index;
+  }
+  else
+  {
+    if(compare(*left_child, *right_child))
+    {
+      ans = left_child;
+      ans_index = left_child_index;
+    }
+    else
+    {
+      ans = right_child;
+      ans_index = right_child_index;
+    }
+  }
+
+
+  
+  while(parent_index < nodes && compare(*ans, *parent))
+  {
+    swap(*ans, *parent);
+    parent_index = ans_index;
+    parent = ans;
+    left_child_index = 2 * parent_index + 1;
+    left_child = begin + left_child_index;
+    right_child_index = 2 * parent_index + 2;
+    right_child = begin + right_child_index;
+    swaps++;
+    if(right_child_index >= nodes)
+    {
+      ans = left_child;
+      ans_index = left_child_index;
+    }
+    else
+    {
+      if(compare(*left_child, *right_child))
+      {
+        ans = left_child;
+        ans_index = left_child_index;
+      }
+      else
+      {
+        ans = right_child;
+        ans_index = right_child_index;
+      }
+    }
+  }
+
+  return swaps;
 }
 
 template <typename T, typename CONTAINER, typename COMPARISON>
 void heap_insert(CONTAINER & heapdata, size_t &nodes, T key, COMPARISON compare) {
-  // XXX You must implement this properly.
+  auto root = key;
+  auto left_child = 2 * key + 1;
+  auto right_child = 2 * key + 2;
+
+  if(compare())
+  {
+    root = left_child
+  } 
+
+  if(right_child < nodes && heapdata[right_child] > heapdata[root])
+  {
+    root = right_child
+  }
+
+  if(root != key)
+  {
+    swap(heapdata[key], heapdata[root] );
+
+    heap_insert(heapdata, nodes, );
+  }
+
 }
 
 template <typename CONTAINER, typename COMPARISON>
